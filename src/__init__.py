02012b26993432989c457a93dde64bc05425e2ff
hsaraiya5 <40361539+hsaraiya5@users.noreply.github.com>
2024-07-10 22:02:15 -0400

    feat: finished login flow, added DB

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
     
     # secret key for the app, as it relates to cookies/sessions
     app.config['SECRET_KEY'] = 'akjgkrskdb'
+
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    
 
     from .views import views
     from .auth import auth
@@ -13,6 +28,28 @@ def create_app():
     app.register_blueprint(auth, url_prefix="/")
 
 
+    # import models file to create the relations
+    from .models import User, Note
+
+
+    if not path.exists('src/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print("Created Database!")
+
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
 
+
+    
+
+    
